2057ff9ba7f85341830f50f44189ada3db2b6390
Joe Haas <joe.jbh@pm.me>
2023-07-06 22:29:29 -0500

    Improve look of site

--- spec/views/locations/index.html.erb_spec.rb
+++ spec/views/locations/index.html.erb_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "locations/index", type: :view do
         address: "Street Address",
         city: "City",
         state: "State",
-        zip: "11111",
+        zip: "22222",
         ip_address: "2.2.2.222"
       ),
       Location.create!(
@@ -15,19 +15,15 @@ RSpec.describe "locations/index", type: :view do
         city: "City",
         state: "State",
         zip: "22222",
-        ip_address: "2.2.2.222"
       )
     ])
   end
 
   it "renders a list of locations" do
     render
-    cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new("Street Address".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("City".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("State".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("Zip".to_s), count: 2
-    assert_select cell_selector, text: Regexp.new("IP Address".to_s), count: 2
-    assert_select "form>button", text: Regexp.new("Delete".to_s), count: 2
+    assert_select make_test_id('address_line'), text: Regexp.new("Street Address".to_s), count: 2
+    assert_select make_test_id('city_state_zip'), text: "City, State 22222", count: 2
+    assert_select make_test_id('ip_address'), text: Regexp.new("2.2.2.222".to_s), count: 1
+    assert_select make_test_id('delete'), text: "Delete", count: 2
   end
 end
